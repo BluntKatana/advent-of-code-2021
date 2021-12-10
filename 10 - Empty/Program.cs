a326ef67f9dab1f57ba637e06a2603930e7a4bc9
Floris Bos <florisbos1204@gmail.com>
2021-12-10 10:58:01 +0100

    Init

---
+++ 10 - Empty/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        public static Dictionary<char, char> charPairs    = new Dictionary<char, char>()  { { ')','(' },  { ']', '[' },   { '}', '{' },   { '>', '<' } };
+        public static Dictionary<char, int> charCostPart1 = new Dictionary<char, int>()   { { ')', 3 },   { ']', 57 },    { '}', 1197 },  { '>', 25137 } };
+        public static Dictionary<char, int> charCostPart2 = new Dictionary<char, int>()   { { '(', 1 },   { '[', 2 },     { '{', 3 },     { '<', 4 } };
+
+        static void Main(string[] args)
+        {
+            List<long> totalScores = new List<long>();
+            string currLine = Console.ReadLine();
+            
+            while (currLine != ":q")
+            {
+                // Remove legal chunks from line.
+                string[] legalChunks = new[] { "()", "[]", "{}", "<>" };
+                while (legalChunks.Any(s => currLine.Contains(s)));
+                    currLine = currLine.Replace("()", "").Replace("[]", "").Replace("{}", "").Replace("<>", "");
+
+                // Check for corrupted lines.
+                if (checkWrongCharPoints(currLine) == 0)
+                    totalScores.Add(getScore(currLine));
+
+                // Read new line.
+                currLine = Console.ReadLine();
+            }
+
+            // Print result.
+            totalScores.Sort();
+            Console.WriteLine("Result: " + totalScores[totalScores.Count / 2]);
+        }
+
+        public static long getScore(string line)
+        {
+            long totalScore = 0;
+            foreach (char c in line.Reverse())
+                totalScore = totalScore * 5 + charCostPart2[c];
+
+            return totalScore;
+        }
+
+        public static int checkWrongCharPoints(string line)
+        {
+            for (int i = 1; i < line.Length; i++)
+            {
+                char prevChar = line[i - 1]; char currChar = line[i]; 
+                if (!charPairs.ContainsKey(prevChar) && charPairs.ContainsKey(currChar))
+                    if (charPairs[currChar] != prevChar)
+                        return charCostPart1[currChar];
+            }
+            return 0;
+        }
+    }
+}
