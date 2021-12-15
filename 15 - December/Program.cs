df76545496f49f58540bf59476190a47537b8011
Floris Bos <florisbos1204@gmail.com>
2021-12-15 14:40:42 +0100

    Part 2 finally

--- 15 - December/Program.cs
+++ 15 - December/Program.cs
@@ -6,7 +6,8 @@ namespace AdventOfCode
 {
     class Program
     {
-        public static int size = 100;
+        public static int size;
+        public static int duplicate = 5;
 
         static void Main(string[] args)
         {
@@ -19,31 +20,48 @@ namespace AdventOfCode
                 currLine = Console.ReadLine();
             }
 
+            size = lines.Count();
+
             // Parse input
-            int[,] grid = new int[size,size];
+            int[,] originalGrid = new int[size,size];
             for (int i = 0; i < lines.Count(); i++)
                 for (int j = 0; j < lines[i].Count(); j++)
-                    grid[i,j] = int.Parse(lines[i][j].ToString());
+                    originalGrid[i,j] = int.Parse(lines[i][j].ToString());
 
-            int[] src = { 0, 0 };
-            int[] dest = { size - 1, size - 1 };
-
+            int[,] biggerGrid = createBiggerGrid(originalGrid, duplicate);
 
             // Do something with the input after runtime.
-            Console.WriteLine("Result: " + shortestPath(grid, size));
+            Console.WriteLine("Result: " + shortestPath(biggerGrid, size * duplicate));
         }
 
-        public static bool isValid(int i, int j)
+        public static int[,] createBiggerGrid(int[,] grid, int amount)
         {
-            return i >= 0 && i < size && j >= 0 && j < size;
+            int[,] biggerGrid = new int[size * amount, size * amount];
+            for (int i = 0; i < size; i++)
+                for (int j = 0; j < size; j++) 
+                    for (int hor = 0; hor < amount; hor++)
+                        for (int ver = 0; ver < amount; ver++)
+                            biggerGrid[i + size * hor, j + size * ver] = wrapAround(grid[i, j] + hor + ver);
+
+            return biggerGrid;
         }
 
-        public static int shortestPath(int[,] grid, int size)
+        public static int wrapAround(int i)
+        {
+            return (i % 9 == 0) ? 9 : i % 9;
+        }
+
+        public static bool isValid(int i, int j, int amount)
+        {
+            return i >= 0 && i < amount && j >= 0 && j < amount;
+        }
+
+        public static int shortestPath(int[,] grid, int amount)
         {
             // Initializing distance array.
-            int[,] distance = new int[size, size];
-            for (int i = 0; i < size; i++)
-                for (int j = 0; j < size; j++)
+            int[,] distance = new int[amount, amount];
+            for (int i = 0; i < amount; i++)
+                for (int j = 0; j < amount; j++)
                     distance[i, j] = int.MaxValue;
 
             int[] dx = { -1, 0, 1, 0 };
@@ -53,10 +71,11 @@ namespace AdventOfCode
             List<Node> set = new List<Node>();
             set.Add(new Node(0, 0, 0));
             distance[0, 0] = 0;
-
-            while (distance[size - 1, size - 1] == int.MaxValue)
+             
+            while (distance[amount - 1, amount - 1] == int.MaxValue)
             {
                 // Get the node with minimum distance and delete it.
+                set.Sort((Node a, Node b) => a.dist.CompareTo(b.dist));
                 int min = set.Min(n => n.dist);
                 Node k = set.Find(n => n.dist == min);
                 set.Remove(k);
@@ -66,7 +85,7 @@ namespace AdventOfCode
                     int x = k.x + dx[i];
                     int y = k.y + dy[i];
 
-                    if (!isValid(x, y))
+                    if (!isValid(x, y, amount))
                         continue;
 
                     if (distance[x, y] > distance[k.x, k.y] + grid[x, y])
@@ -80,17 +99,14 @@ namespace AdventOfCode
                 }
             }
 
-            return distance[size - 1, size - 1];
+            return distance[amount - 1, amount - 1];
 
         }
     }
 
     class Node
     {
-
         public int dist, x, y;
-
-        // Constructor
         public Node(int x, int y,
                     int dist)
         {
