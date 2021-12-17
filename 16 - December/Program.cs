c1cad4348011299209ba38421623326ad24da73e
Floris Bos <florisbos1204@gmail.com>
2021-12-17 09:31:17 +0100

    çleanup

--- 16 - December/Program.cs
+++ 16 - December/Program.cs
@@ -6,81 +6,113 @@ namespace AdventOfCode
 {
     class Program
     {
-        public static Queue<string> queue;
+        public static long versionTotal = 0;
         static void Main(string[] args)
         {
 
-            string hexaPackets = Console.ReadLine();
-            string binaryPackets = String.Join(String.Empty,
-                hexaPackets.Select(
+            string hexaNums = Console.ReadLine();
+            string binaryNums = String.Join(String.Empty,
+                hexaNums.Select(
                     c => Convert.ToString(Convert.ToInt32(c.ToString(), 16), 2).PadLeft(4, '0')
                     )
                 );
 
-            queue = new Queue<string>();
-            
-            foreach (char c in binaryPackets)
+            Queue<string> queue = new Queue<string>();
+            foreach (char c in binaryNums)
                 queue.Enqueue(c.ToString());
 
-            int total = 0;
-
-            while (queue.Count() > 0)
-            {
-                string versionString = dequeueToString(3);
-                string typeIDString = dequeueToString(3);
-                int version = Convert.ToInt32(versionString, 2);
-                int typeID = Convert.ToInt32(typeIDString, 2);
-                total += version;
-
-                // Literal value
-                if (typeID == 4)
-                {
-                    string literalValueBits = "";
-                    string fiveBits = dequeueToString(5);
-                    int discardBits = 1;
-                    do
-                    {
-                        literalValueBits += fiveBits[1..fiveBits.Length];
-                        fiveBits = dequeueToString(5);
-                        discardBits += 1;
-                    } while (fiveBits[0] != '0');
-                    literalValueBits += fiveBits[1..fiveBits.Length];
-                    int literalValue = Convert.ToInt32(literalValueBits, 2);
-                    _ = dequeueToString(discardBits % 4);
-                } else 
-                // Operator
-                {
-                    string lengthType = queue.Dequeue();
-                    if (lengthType == "0")
-                    {
-                        string subPacketsLengthBits = dequeueToString(15);
-                        int subPacketsLength = Convert.ToInt32(subPacketsLengthBits, 2);
-                        _ = dequeueToString(subPacketsLength);
-                    } else
-                    {
-                        string numberOfSubPacketsBits = dequeueToString(11);
-                        int numberOfSubPackets = Convert.ToInt32(numberOfSubPacketsBits, 2);
-                        for (int i = 0; i < numberOfSubPackets;i++)
-                            _ = dequeueToString(11);
-
-                    }
-                }                
-            }
-
+            long total = header(queue);
             Console.WriteLine("Result: " + total);
         }
 
-        public static string dequeueToString(int i)
+        public static long header(Queue<string> queue)
+        {
+            long version = dequeueToInt(3, queue);
+            long typeID = dequeueToInt(3, queue);
+
+            // Part 1
+            versionTotal += version;
+
+            if (typeID == 4)
+                return literalPacket(queue);
+            else
+                return operatorPacket(queue, typeID);
+        }
+
+        public static long literalPacket(Queue<string> queue)
+        {
+            long literalValue = 0;
+            string checkBit = dequeueToString(1, queue);
+            while (checkBit != "0")
+            {
+                long fourBits = dequeueToInt(4, queue);
+                literalValue = literalValue * 16 + fourBits;
+                checkBit = dequeueToString(1, queue);
+            }
+            return literalValue * 16 + dequeueToInt(4, queue);
+        }
+
+        public static long operatorPacket(Queue<string> queue, long typeID)
+        {
+            string lengthType = queue.Dequeue();
+            List<long> subPacketTotals = new List<long>();
+
+            if (lengthType == "0")
+            {
+                // Length based packets
+                long subPacketsLength = dequeueToInt(15, queue);
+
+                Queue<string> subPackets = new Queue<string>();
+                string subPacketsString = dequeueToString(subPacketsLength, queue);
+                foreach (char c in subPacketsString)
+                    subPackets.Enqueue(c.ToString());
+
+                while (subPackets.Count() > 0)
+                    subPacketTotals.Add(header(subPackets));
+            }
+            else
+            {
+                // Number based packets
+                long numberOfSubPackets = dequeueToInt(11, queue);
+                for (long i = 0; i < numberOfSubPackets; i++)
+                    subPacketTotals.Add(header(queue));
+            }
+
+            switch (typeID) {
+                case 0:
+                    return subPacketTotals.Sum();
+                case 1:
+                    return subPacketTotals.Aggregate((total, next) => total * next);
+                case 2:
+                    return subPacketTotals.Min();
+                case 3:
+                    return subPacketTotals.Max();
+                case 5:
+                    return subPacketTotals[0] > subPacketTotals[1] ? 1 : 0;
+                case 6:
+                    return subPacketTotals[0] < subPacketTotals[1] ? 1 : 0;
+                default: // case 7
+                    return subPacketTotals[0] == subPacketTotals[1] ? 1 : 0;
+            }
+        }
+
+
+        public static string dequeueToString(long i, Queue<string> queue)
         {
             return String.Join("", queue.DequeueChunk(i).ToArray());
+        }
+
+        public static long dequeueToInt(long i, Queue<string> queue)
+        {
+            return Convert.ToInt32(String.Join("", queue.DequeueChunk(i).ToArray()), 2);
         }
     }
 
     public static class QueueExtensions
     {
-        public static IEnumerable<T> DequeueChunk<T>(this Queue<T> queue, int chunkSize)
+        public static IEnumerable<T> DequeueChunk<T>(this Queue<T> queue, long chunkSize)
         {
-            for (int i = 0; i < chunkSize && queue.Count > 0; i++)
+            for (long i = 0; i < chunkSize && queue.Count > 0; i++)
             {
                 yield return queue.Dequeue();
             }
