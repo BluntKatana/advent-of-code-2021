4c5876ee5f90e789be790c8e281843b41eb608e4
Floris Bos <florisbos1204@gmail.com>
2021-12-22 10:11:48 +0100

    almost part 2

--- 21 - December/Program.cs
+++ 21 - December/Program.cs
@@ -16,30 +16,45 @@ namespace AdventOfCode
 
             List<Player> players = new List<Player>() { one, two };
 
-            doTurn(players, 1, 0);
+            foreach (int die in diceThrows())
+                doTurn(players, 0, die);
+
+            Console.WriteLine("Player 1: " + players[0].wins);
+            Console.WriteLine("Player 2: " + players[1].wins);
         }
-        public static void doTurn(List<Player> players, int die, int currP)
+        public static void doTurn(List<Player> players, int currP, int die)
         {
-            players[currP % 2].doMove(die, die + 1, die + 2);
+
+            players[currP % 2].doMove(die);
 
             // Check for win
-            if (players[currP % 2].score >= 1000)
+            if (players[currP % 2].score >= 21)
             {
-                long res = die + 2;
-                Console.WriteLine("Result: " + (res * players[currP % 2 + 1].score).ToString());
+                // Add state to cache
+                players[currP % 2].wins++;
                 return;
             }
 
-            doTurn(players, die + 3, currP % 2 + 1);
+            // Do all the other possible moves
+            foreach (int i in diceThrows()) 
+                doTurn(players, currP % 2 + 1, i);
         }
+        public static List<int> diceThrows()
+        {
+            List<int> distinctDiceThrows = new List<int>();
+            for (int i = 1; i <= 3; i++)
+                for (int j = 1; j <= 3; j++)
+                    for (int k = 1; k <= 3; k++)
+                        distinctDiceThrows.Add(i + j + k);
 
+            return distinctDiceThrows;
+
+        }
     }
-
-
 
     class Player
     {
-        public int position, score, wins;
+        public long position, score, wins;
         public Player(int position, int score, int wins)
         {
             this.position   = position;
@@ -47,10 +62,10 @@ namespace AdventOfCode
             this.wins       = wins;
         }
 
-        public void doMove(int firstRoll, int secondRoll, int thirdRoll)
+        public void doMove(int throws)
         {
             // all them rolls
-            position += firstRoll + secondRoll + thirdRoll;
+            position += throws;
 
             // do wrap around
             while (position > 10)
